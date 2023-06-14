b19e80b4e799a6d8d05161c2b09da5eac04400b3
Prasad <prasad@lvi.co.jp>
2023-06-14 11:58:49 +0100

    test

--- src/main/java/com/example/mockapi/GreetingController.java
+++ src/main/java/com/example/mockapi/GreetingController.java
@@ -1,6 +1,7 @@
 package com.example.mockapi;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,8 +13,13 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @GetMapping("/greeting")
-    public Review greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+    @GetMapping("/ask")
+    public Review ask(@RequestParam(value = "name", defaultValue = "World") String name) {
         return new Review("test");
+    }
+
+    @PostMapping("/ask")
+    public Review ask(Query query) {
+        return new Review("post response : " + query.getQuery());
     }
 }
---
+++ src/main/java/com/example/mockapi/Query.java
@@ -0,0 +1,11 @@
+package com.example.mockapi;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Query
+{
+    String query;
+}
